ffec35deaa7450f4e5b8095d1bba71aa80a76daa
Iain <iain@polevaultweb.com>
2019-06-27 15:25:26 +0100

    Fix alert bar CTA button padding when wrapping under text.

--- templates/alert-bar.php
+++ templates/alert-bar.php
@@ -11,10 +11,10 @@ if ( $alert_bar->is_sticky ) {
 <div class="alert-bar<?php echo $class; ?>" data-slug="<?php echo $alert_bar->key; ?>-alert-bar" style="display: none;">
 	<p>
 		<?php echo $alert_bar->message; ?>
-		<?php if ( $alert_bar->cta ) : ?>
-			<a class="dbi-btn" target="<?php echo $alert_bar->cta['target']; ?>" href="<?php echo $alert_bar->cta['url']; ?>"><?php echo empty( $alert_bar->cta['title'] ) ? 'Click for Details' : $alert_bar->cta['title']; ?></a>
-		<?php endif; ?>
 	</p>
+	<?php if ( $alert_bar->cta ) : ?>
+		<a class="dbi-btn" target="<?php echo $alert_bar->cta['target']; ?>" href="<?php echo $alert_bar->cta['url']; ?>"><?php echo empty( $alert_bar->cta['title'] ) ? 'Click for Details' : $alert_bar->cta['title']; ?></a>
+	<?php endif; ?>
 
 	<a href="#" class="dismiss"></a>
 </div>
